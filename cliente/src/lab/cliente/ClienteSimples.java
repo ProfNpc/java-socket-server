5b488a9fc99aad551c563fc389933f6f106f2b41
Programador2 <programador2@gmail.com>
2024-07-30 22:43:29 -0300

    Versão Inicial

---
+++ cliente/src/lab/cliente/ClienteSimples.java
@@ -0,0 +1,46 @@
+package lab.cliente;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClienteSimples {
+
+	public static void main(String[] args) {
+
+		try {
+			System.out.println("[Cliente] Inicio");
+
+			//Cria um objeto socket que se conecta na porta 7777 na maquina de endereço localhost
+			Socket clientSocket = new Socket("localhost", 7777);
+
+			//Cria um fluxo de saída (output stream) que permitira
+			//enviar dados para a outra ponta do socket (servidor)
+			PrintWriter saida = new PrintWriter(clientSocket.getOutputStream(), true);
+			
+			//Cria um fluxo de entrada (input stream) que permitira ler
+			//o que for eviado pela outra ponta do socket (servidor)
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			System.out.println("[Cliente] Vai enviar 'quem é?'");
+			//Aqui a mensagem é enviada para a outra ponta do socket(servidor)
+			saida.println("quem é?");
+		
+			//Aqui a resposta enviada pelo servidor pelo socket é lida
+			String resposta = entrada.readLine();
+			
+			System.out.println("[Cliente] Resposta:" + resposta);
+			
+			//Fecha e libera os recursos utilizados
+			saida.close();
+			entrada.close();
+			clientSocket.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("[Cliente] Fim");
+		}
+	}
+}
---
+++ servidor/src/lab/servidor/ServidorRustico.java
@@ -0,0 +1,63 @@
+package lab.servidor;
+
+//import java.io.*;
+//import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorRustico {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("[ServidorRustico] Inicio");
+			//Cria um objeto ServerSocket para ficar "ouvindo" a porta 7777
+			//aguardando que outro programa se conecte nessa porta
+			ServerSocket server = new ServerSocket(7777);
+
+			//Recupera o endereço IP da maquina que esta rodando esse programa
+			InetAddress enderecoIP = server.getInetAddress();
+			//Recupera a porta, que nós sabemos que é a 7777
+			int porta = server.getLocalPort();
+
+			//Recupera, salva e apresenta a maquina, IP e a porta onde esta a aplicacao
+			String serverIdentification = String.format("IP:=%s, Porta:%s\n", InetAddress.getLocalHost(), porta);
+			System.out.printf("[ServidorRustico] %s", serverIdentification);
+			
+			
+			//Fica aguardando uma conexão
+			Socket socket = server.accept();
+
+			//Cria um fluxo de saída (output stream) que permitira
+			//enviar dados para a outra ponta do socket (cliente)
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			
+			//Cria um fluxo de entrada (input stream) que permitira ler
+			//o que for eviado pela outra ponta do socket (cliente)
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			//Lê o que o cliente enviou
+			String str = in.readLine();
+			
+			//Verifica se é o texto esperado
+			if (str.equals("quem é?")) {
+				System.out.println("[ServidorRustico] Recebeu '" + str + "'");
+
+				//Responde para o cliente
+				out.println(serverIdentification);
+			}
+
+			//Fecha e libera os recursos utilizados
+			in.close();
+			out.close();
+			server.close();
+		}catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			System.out.println("[ServidorRustico] Fim");
+		}
+	}
+}
